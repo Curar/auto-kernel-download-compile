cb2a9a53e26a419c3497786cc5104ddb78610c9b
Wojtek <wojtekg4455@gmail.com>
2020-10-29 19:59:57 +0100

    Komentarz
    Zmiana poprawek wynikających z chwilowej edycji w innym programie niż
    Vim.

--- auto-kernel-1.0.sh
+++ auto-kernel-1.0.sh
@@ -53,13 +53,12 @@ function curl_gpg_sudo_exist() {
 		exit 1
     }
 	elif ! [ -x "$(command -v gpg)" ]; then {
-	    echo 'UWAGA gpg nie jest zainstalowane !' >&2
+		echo 'UWAGA gpg nie jest zainstalowane !' >&2
        	exit 1
     } fi	       
 }
 
-function pauza() {
-	echo ""	
+function pauza() {	
 	read -p "Naduś klawisz ENTER aby kontynować ..."
 }
 
@@ -87,63 +86,58 @@ function rdzenie() {
 	)
 	select opcja in "${opcje_wyboru[@]}"
 	do
-			case $opcja in
-			"Pobrać tylko wskazane źródło kernela") 	
-			    echo -e "\e[32mPracujesz jako :\e[0m"; whoami 
-                echo -e "\e[33mPodaj wersję kernela którą mam skompilować np.: 5.9.2\e[0m"
-                read KERNEL
-                zmienne;
-                rdzenie;
+		case $opcja in
+		"Pobrać tylko wskazane źródło kernela") 	
+			echo -e "\e[32mPracujesz jako :\e[0m"; whoami 
+                	echo -e "\e[33mPodaj wersję kernela którą mam skompilować np.: 5.9.2\e[0m"
+                	read KERNEL
+                	zmienne;
+                	rdzenie;
                         if [ ! -e "$KERNEL_EXIST" ] && [ ! -e "$KERNEL_SIGN" ]; then {
-		                    if curl --output /dev/null --silent --head --fail "$ADRES_KERNELA"; then {
+		         	if curl --output /dev/null --silent --head --fail "$ADRES_KERNELA"; then {
 			                echo -e "\e[32m Kernel istnieje : $ADRES_KERNELA , pobieram :\e[0m"
 			                sleep 3			
 			                curl --compressed --progress-bar -o "$KERNEL_EXIST" "$ADRES_KERNELA"
 			                curl --compressed --progress-bar -o "$KERNEL_SIGN" "$ADRES_PODPISU"
 			                clear
-                            curl_gpg_sudo_exist;
-                            echo "Pobierma klucze GPG"
-	                        gpg --locate-keys torvalds@kernel.org gregkh@kernel.org
-	                        unxz -c linux-${KERNEL}.tar.xz | gpg --verify linux-${KERNEL}.tar.sign -
-	                            if [ $? -eq 0 ]; then {
-                                    echo -e "\e[32m=====================\e[0m"
-                                    echo -e "\e[32m=  Podpis poprawny  =\e[0m"
-                                    echo -e "\e[32m=====================\e[0m"	
-                                    sleep 3	
-                                } else {
-    		                        echo "Problem z podpisem : linux-${KERNEL}.tar.xz"
-                                } fi
-                            }
-		                    else {
-  			                echo "Kernel nie istnieje : $ADRES_KERNELA"
-			                exit
-                            } fi
+                            		curl_gpg_sudo_exist;
+                            		echo "Pobierma klucze GPG"
+	                        	gpg --locate-keys torvalds@kernel.org gregkh@kernel.org
+	                        	unxz -c linux-${KERNEL}.tar.xz | gpg --verify linux-${KERNEL}.tar.sign -
+	                            		if [ $? -eq 0 ]; then {
+                                    		echo -e "\e[32m=====================\e[0m"
+                                    		echo -e "\e[32m=  Podpis poprawny  =\e[0m"
+                                    		echo -e "\e[32m=====================\e[0m"	
+                                   	 	sleep 3	
+                                		} else {
+    		                        	echo "Problem z podpisem : linux-${KERNEL}.tar.xz"
+                                		} fi
+                            	}
+		                else {
+  			             echo "Kernel nie istnieje : $ADRES_KERNELA"
+			             exit
+                            	} fi
                         }
-	                    else {
-	                        echo -e "\e[32m===========================\e[0m"
-	                        echo -e "\e[32m= Kernel jest już pobrany =\e[0m"
-	                        echo -e "\e[32m===========================\e[0m"
-	                        sleep 3
+	                else {
+	                     echo -e "\e[32m===========================\e[0m"
+	                     echo -e "\e[32m= Kernel jest już pobrany =\e[0m"
+	                     echo -e "\e[32m===========================\e[0m"
+	                     sleep 3
                         } fi
 			;;
-            "Pobrać i skompilować wskazane źródło")
-            ;;
-            "Sprawdzić dostępne kernele z kernel.org")
-            ;;
-			"Wyjście")
-				clear
-				exit 1
-			;;
+            	"Pobrać i skompilować wskazane źródło")
+            	;;
+            	"Sprawdzić dostępne kernele z kernel.org")
+            	;;
+		"Wyjście")
+			clear
+			exit 1
+		;;
 	*) echo "Brak wyboru !"
 	esac
 	break
 done
 }
 echo -e "\e[32mBy Curar :) 2020 r.\e[0m"
-unset PORTY
 pauza
 done
-
-
-
-
