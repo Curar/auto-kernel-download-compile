74ee9656a0fcd1f73b8ec52e6bce0983aa011023
wojtekg4455@gmail.com <wojtekg4455@gmail.com>
2020-12-03 14:58:10 +0100

    Komentarz
    Dodano pahole do wymaganych aplikacji podczas kompilacji kernela.

--- auto-kernel-1.0.sh
+++ auto-kernel-1.0.sh
@@ -50,7 +50,7 @@ ADRES_KERNELA="https://cdn.kernel.org/pub/linux/kernel/v5.x/${wybor}"
 function polecenia() {
 echo -e "\e[33mSprawdzę czy masz odpowiednie programy w systemie\e[0m"
 sleep 2
-for program in which sudo rsync sed patch make m4 gzip groff grep gettext gcc flex file fakeroot bison bc automake autoconf; do
+for program in pahole which sudo rsync sed patch make m4 gzip groff grep gettext gcc flex file fakeroot bison bc automake autoconf; do
       	printf '%-10s' "$program"
   if hash "$program" 2>/dev/null; then
     echo -e "\e[32m- Jest zainstalowany\e[0m"
