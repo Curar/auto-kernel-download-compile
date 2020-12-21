715770d127fbd5e9f1195c0ed8784c22209b00ac
wojtekg4455@gmail.com <wojtekg4455@gmail.com>
2020-12-21 14:17:16 +0100

    Komentarz
    Dodano sól w postaci dyfiniowania zmiennej : scripts/config --set-str CONFIG_BUILD_SALT "$SALT"

--- auto-kernel-1.0.sh
+++ auto-kernel-1.0.sh
@@ -88,7 +88,7 @@ function rodzaje_kompilacji() {
 		  "localyesconfig") make localyesconfig;;		  
 		  "localmodconfig") make localmodconfig;;
 		  "tinyconfig") make tinyconfig;;
-		  "R-A-K-I-E-T-K-A") echo -e "\e[32m${tablica_rakietka["0"]}\e[0m" && cd .. && pwd && cp $CONFIG $katalog && cd $katalog;;
+		  "R-A-K-I-E-T-K-A") echo -e "\e[32m${tablica_rakietka["0"]}\e[0m" && cd .. && pwd && cp $CONFIG $katalog && cd $katalog && scripts/config --set-str CONFIG_BUILD_SALT "$SALT";;
 		  "WYJŚCIE") exit 1;;
 	  	  *) echo "Brak wyboru"
 	  esac
